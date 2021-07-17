e9f5801b9ac5b6f230eeccf47803a2b14ae5723c
Filip Culig <filip.culig@gmail.com>
2021-07-17 15:16:54 +0200

    Removed everything from login storyboard

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -10,42 +10,8 @@ import UIKit
 
 class LoginViewController : UIViewController {
     
-    @IBOutlet private weak var touchCounterLabel: UILabel!
-    @IBOutlet private weak var touchCounterButton: UIButton!
-    @IBOutlet private weak var loadingIndicator: UIActivityIndicatorView!
-    @IBOutlet private weak var loadingIndicatorLabel: UILabel!
-    
-    private var numberOfClicks: Int = 0
-    
     override func viewDidLoad(){
         super.viewDidLoad()
-        
-        showLoadingScreen()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
-            self.hideLoadingScreen()
-        })
-    }
-    
-    @IBAction private func buttonPressed() {
-        numberOfClicks += 1
-        configureUI()
-    }
-    
-    private func configureUI(){
-        touchCounterLabel.text = String(numberOfClicks)
-    }
-    
-    private func showLoadingScreen(){
-        loadingIndicator.startAnimating()
-        touchCounterLabel.isHidden = true
-        touchCounterButton.isHidden = true
-    }
-    
-    private func hideLoadingScreen(){
-        loadingIndicator.stopAnimating()
-        loadingIndicatorLabel.isHidden = true
-        touchCounterLabel.isHidden = false
-        touchCounterButton.isHidden = false
     }
     
 }
