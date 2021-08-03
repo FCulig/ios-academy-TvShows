150e489e4527c38a9a0ea673e30df9dd619dbaab
Filip Culig <filip.culig@gmail.com>
2021-08-04 00:08:09 +0200

    Added fetching user profile images

--- TV Shows/TV Shows/Views/ShowDetails/TVShowReviewTableViewCell.swift
+++ TV Shows/TV Shows/Views/ShowDetails/TVShowReviewTableViewCell.swift
@@ -14,16 +14,26 @@ class TVShowReviewTableViewCell: UITableViewCell {
     
     @IBOutlet private weak var reviewersEmailLabel: UILabel!
     @IBOutlet private weak var reviewLabel: UILabel!
+    @IBOutlet private weak var profileImage: UIImageView!
     
     override func prepareForReuse() {
         super.prepareForReuse()
         reviewersEmailLabel.text = ""
         reviewLabel.text = ""
+        profileImage.image = UIImage(named: "ic-profile-placeholder")
     }
     
     func configure(review: Review) {
         reviewersEmailLabel.text = review.user.email
         reviewLabel.text = review.comment
+        guard let imageUrl = review.user.imageUrl else {
+            profileImage.image = UIImage(named: "ic-profile-placeholder")
+            return
+        }
+        profileImage.kf.setImage(
+            with: URL(string: imageUrl),
+            placeholder: UIImage(named: "ic-profile-placeholder")
+        )
     }
     
 }
